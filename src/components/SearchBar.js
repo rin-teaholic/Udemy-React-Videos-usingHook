c0faad814d1b0aea82549e36d4b0f9f006891f2b
Rin <info@rin-m.site>
2021-08-29 16:33:33 +0900

    Deploy Vercel

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ export const SearchBar = ({ onFormSubmit }) => {
             <div className="search-bar ui segment">
                 <form onSubmit={onSubmit} className="ui form">
                     <div className="field">
-                        <label>Video Search</label>
+                        <label>Find a Video!</label>
                         <input 
                         type="text" 
                         value={term}
